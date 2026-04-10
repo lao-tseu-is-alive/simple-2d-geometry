330da3387773e0ed33cde13f40e0049f4a3f2b36
cgil <lao.tseu.is.alive@gmail.com>
2026-04-10 10:37:40 +0200

    corrected export errors

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,36 @@
-import Angle, {type AngleType } from "./Angle.ts";
+import Angle, {type AngleType} from "./Angle.ts";
 import {
-  EPSILON,
-  PRECISION,
-  isNumeric,
-  roundNumber,
-  getNumberOrFail,
-  fixDec,
+    EPSILON,
+    PRECISION,
+    isNumeric,
+    roundNumber,
+    getNumberOrFail,
+    fixDec,
 } from "./Geometry.ts";
-import Line from "./Line.ts";
-import Point, {type iPoint } from "./Point.ts";
-import Triangle from "./Triangle.ts";
-import { APP, VERSION, BUILD_DATE } from "./version.ts";
+import Point, {type iPoint, type coordinate2dArray} from "./Point.ts";
+import Line, {type LineInterface, type coordinatesLineArray} from "./Line.ts";
+import Triangle, {type TriangleInterface, type coordinatesTriangleArray} from "./Triangle.ts";
+import Circle, {type CircleInterface} from "./Circle.ts";
+import {APP, VERSION, BUILD_DATE} from "./version.ts";
 
 export {
-  APP,
-  Angle,
-  BUILD_DATE,
-  Point,
-  Line,
-  Triangle,
-  EPSILON,
-  PRECISION,
-  isNumeric,
-  roundNumber,
-  getNumberOrFail,
-  fixDec,
-  VERSION,
+    APP,
+    Angle,
+    BUILD_DATE,
+    Point,
+    Line,
+    Triangle,
+    Circle,
+    EPSILON,
+    PRECISION,
+    isNumeric,
+    roundNumber,
+    getNumberOrFail,
+    fixDec,
+    VERSION,
 };
-export type { iPoint, AngleType };
+export type {
+    AngleType, iPoint, coordinate2dArray,
+    TriangleInterface, coordinatesTriangleArray, CircleInterface,
+    LineInterface, coordinatesLineArray
+};
