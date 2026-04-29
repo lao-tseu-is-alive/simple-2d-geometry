5f7b0462b3ff0b7fe11f75261ff418f6cd2ffbc1
cgil <lao.tseu.is.alive@gmail.com>
2026-04-29 16:37:55 +0200

    code cleanup

--- build.ts
+++ build.ts
@@ -1,6 +1,5 @@
-import fs from "fs";
-import {$} from "bun";
-import {name, version} from "./package.json";
+import { $ } from "bun";
+import { version } from "./package.json";
 
 console.log(`🐰 🏗️  Building workspace packages v${version}...`);
 
@@ -9,30 +8,30 @@ console.log(`🐰 🧹 Cleanup of old ${outputDir} content...`);
 await $`rm -rf ${outputDir}`;
 
 const buildPackage = async (pkgName: string, entrypoint: string, hasBrowser: boolean = false) => {
-    const pkgFileName = pkgName.split('/').pop()!;
-    const pkgOutputDir = `${outputDir}/${pkgFileName}`;
-    const sharedConfig = {
-      entrypoints: [entrypoint],
-      outdir: pkgOutputDir,
-      minify: true,
-      external: ["lit", "@lao-tseu-is-alive/geom-2d-core"],
-      sourcemap: "external" as const,
-    };
+  const pkgFileName = pkgName.split("/").pop()!;
+  const pkgOutputDir = `${outputDir}/${pkgFileName}`;
+  const sharedConfig = {
+    entrypoints: [entrypoint],
+    outdir: pkgOutputDir,
+    minify: true,
+    external: ["lit", "@lao-tseu-is-alive/geom-2d-core"],
+    sourcemap: "external" as const,
+  };
 
-    console.log(`🐰 📦 Generating bundles for ${pkgName}...`);
-    const builds = [
-      await Bun.build({...sharedConfig, format: "esm", naming: `${pkgFileName}.esm.js`}),
-      await Bun.build({...sharedConfig, format: "cjs", naming: `${pkgFileName}.cjs.js`})
-    ];
-    if (hasBrowser) {
-      builds.push(await Bun.build({
-          ...sharedConfig,
-          entrypoints: [entrypoint.replace('index.ts', 'browser.ts')],
-          format: "iife",
-          naming: `${pkgFileName}.umd.js`
-      }));
-    }
-    await Promise.all(builds);
+  console.log(`🐰 📦 Generating bundles for ${pkgName}...`);
+  const builds = [
+    await Bun.build({ ...sharedConfig, format: "esm", naming: `${pkgFileName}.esm.js` }),
+    await Bun.build({ ...sharedConfig, format: "cjs", naming: `${pkgFileName}.cjs.js` }),
+  ];
+  if (hasBrowser) {
+    builds.push(await Bun.build({
+      ...sharedConfig,
+      entrypoints: [entrypoint.replace("index.ts", "browser.ts")],
+      format: "iife",
+      naming: `${pkgFileName}.umd.js`,
+    }));
+  }
+  await Promise.all(builds);
 };
 
 await buildPackage("@lao-tseu-is-alive/geom-2d-core", "./packages/geom-2d-core/src/index.ts", true);
